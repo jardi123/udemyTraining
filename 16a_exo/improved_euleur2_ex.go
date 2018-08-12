b75a985f8fda1d59249ba64bb8e78f0e9b0b8249
Jardi123 <jardinet.pj@gmail.com>
2018-08-12 21:10:24 +0200

    added some information on bitwise operations

--- 16a_exo/improved_euleur2_ex.go
+++ 16a_exo/improved_euleur2_ex.go
@@ -11,10 +11,27 @@ func main() {
 func FibEuleur() int {
 	sum := 0
 	for cur, prev := 1, 1; cur <= 4000000; cur, prev = cur+prev, cur {
-    // it sets cur to cur + prev and prev to cur at the same time
-    if cur%2 == 0 {
+		// it sets cur to cur + prev and prev to cur at the same time
+		if cur%2 == 0 {
 			sum += cur
 		}
 	}
 	return sum
 }
+
+/*
+i%2== 0 is the same as i&1==0 (bitwise and)
+
+Because
+
+1 in binary as a uint8 is `00000001`
+2 in binary as a uint8 is `00000010`
+3 in binary as a uint8 is `00000011`
+
+so binary wise any odd number has a 1 in first place
+
+i&1 is faster to execute however it can hurt readability
+
+
+
+*/
